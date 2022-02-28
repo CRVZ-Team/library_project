e6c1fa723fbeb5faf47ed4dff5573a83dc158ba1
Roman <58208649+Romes8@users.noreply.github.com>
2022-02-28 17:52:40 +0100

    Added MitID

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -12,6 +12,15 @@ export const LoginPage = () => {
         alert("Log in not implemented");
     } 
 
+    const mitid_style = {
+        border: '2px solid #ccc',
+        borderRadius: '4px',
+        padding: '10px',
+        margin: '10px',
+        width: '25rem',
+        height: '25rem',
+    }
+
     return (
         <div className="container text-center">
             <h1>Log In</h1>
@@ -31,6 +40,20 @@ export const LoginPage = () => {
                 onClick={onLoginClicked}>Log In</button></p>
             <p><button onClick={() => history('/forgot-password')}>Forgot your password?</button></p>
             <p><button onClick={() => history('/signup')} >Don't have an account? Sign Up</button></p>
+
+            <hr />
+
+            <h1>Log In with MitID</h1>
+            {errorMessage && <div className="fail">{errorMessage}</div> }
+            <div className="w-100">
+                <iframe src="https://mitid.eu.pythonanywhere.com/" style={mitid_style}>
+                </iframe>
+            </div>
         </div>
     )
+
+ 
+
+    //"border:2px; height: 20rem; background-color:beige; border-width: 2px; border-color: black;"
+
 }
