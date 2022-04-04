a38b7d564ab201206397bc991c365e931c6302c5
Roman <58208649+Romes8@users.noreply.github.com>
2022-04-04 15:40:28 +0200

    Queried filter options

--- src/components/CatalogSideBar.js
+++ src/components/CatalogSideBar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import ReactPaginate from 'react-paginate';
 import CatalogItem from './CatalogItem';
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,6 +8,28 @@ import Pagination from './general/pagination';
 
 function CatalogSideBar()
 {
+
+    const[authors, setAuthors] = useState([]);
+    const[genres, setGenres] = useState([]);
+    const[years, setYears] = useState([]);
+
+    useEffect(() => {
+        getAuthors();
+    }, []);
+
+
+    const getAuthors = async() => {
+        const res = await axios.get("http://localhost:8080/api/filters/authors");
+        setAuthors(res.data[0].authors);
+        setYears(res.data[1].year_span);
+        setGenres(res.data[2].genres);
+        console.log("years");
+        console.log(res.data[1].year_span[0].max);
+        console.log(res.data[2]);
+
+        console.log(years[0].max);
+    };
+    
     const side = {
         width: '100%',
         paddingLeft: '5%',
@@ -32,88 +55,39 @@ function CatalogSideBar()
                 <h4>Book based</h4>
                 <li class="nav-item" >
                 <h5>Book attributes</h5>
-                <h7>Author</h7>
+                <h7>Authors</h7>
                     <div style={scrollable_small}>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Jozo
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Jozo
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Jozo
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Jozo
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Jozo
-                            </label>
-                        </ul>
+                        {authors.map(author => (
+                            <ul>
+                                <input class="form-check-input" type="checkbox" value={author.name} id="flexCheckDefault"/>
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    {author.name}
+                                </label>
+                            </ul>
+                        ))}                        
                     </div>
                     <h7>Year</h7>
                     <div>
                         <div class="input-group input-group-sm mb-3">
                             <span class="input-group-text" id="inputGroup-sizing-sm">From</span>
-                            <input type="number"  max="2022" min="0" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                            <input type="number" max={years[0].max} min={years[0].min} class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
                         </div>
                         <div class="input-group input-group-sm mb-3">
                             <span class="input-group-text" id="inputGroup-sizing-sm">Till</span>
-                            <input type="number"  max="2022" min="0" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                            <input type="number"   max={years[0].max} min={years[0].min} class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
                         </div>
                     </div>
                     <h7>Genre</h7>
                     <div style={scrollable_small}>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Drama
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Horror
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Sci-fi
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Romance
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Thriller
-                            </label>
-                        </ul>
-                        <ul>
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Detective
-                            </label>
-                        </ul>
+                        {genres.map(genre => (
+                            <ul>
+                                <input class="form-check-input" type="checkbox" value={genre.name} id="flexCheckDefault"/>
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    {genre.name}
+                                </label>
+                            </ul>
+                        ))}
+                        
                     </div>
                 </li>
                 <br/>
