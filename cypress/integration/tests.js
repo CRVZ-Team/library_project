77f2d728b896a412b47f8518bf67eba808a047f0
Roman <58208649+Romes8@users.noreply.github.com>
2022-06-01 22:52:52 +0200

    Working cypress

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,8 +1,6 @@
 describe("Main tests", function () {  //describes collection of the tests
     it("Log in", function () {   //specific test in the collection 
-        setTimeout(function () {
-            cy.visit("localhost:3000/");
-        }, 1000);
+        cy.visit("localhost:3000/");
 
         cy.contains("LOG IN").click();
 
@@ -26,10 +24,8 @@ describe("Main tests", function () {  //describes collection of the tests
     it("Catalog - Click each book", function () {
 
         //check weather user is still loged in
-        setTimeout(() => {
-            cy.contains("LOG OUT");
-            cy.contains("CATALOG").click();
-        }, 2000);
+        cy.contains("LOG OUT");
+        cy.contains("CATALOG").click();
 
         const total_number_of_books = 15
         const pages = Math.ceil(total_number_of_books / 8);
@@ -41,24 +37,22 @@ describe("Main tests", function () {  //describes collection of the tests
             //book loop
             for (let i = starting_book_number; i <= ending_book_number; i++) {
                 //click page each time to get the page for the book
-                setTimeout(() => {
-                    cy.get(".w-25").get(".pagination").find("li").eq(p).click();
+                cy.get(".w-25").get(".pagination").find("li").eq(p).click();
 
                 //get the book
                 cy.get(".g-2").get("#" + i).find(".card-title").then(($div) => {
                     const book = $div.text().split(",")[0];
 
-                        //click the book and verify its correct page and URL 
-                        $div.click();
-                        cy.url().should("include", "/book/" + i);
-                        cy.get(".title-book").should("contain", book);
-                        cy.log("-------- Book active --------");
+                    //click the book and verify its correct page and URL 
+                    $div.click();
+                    cy.url().should("include", "/book/" + i);
+                    cy.get(".title-book").should("contain", book);
+                    cy.log("-------- Book active --------");
 
-                        //return back to main page  -this makes app to go back to page 1-
-                        cy.go("back");
-                    }
-                    );
-                }, 500);
+                    //return back to main page  -this makes app to go back to page 1-
+                    cy.go("back");
+                }
+                );
             }
 
             //set correesponding book numbers for the next page
@@ -75,16 +69,15 @@ describe("Main tests", function () {  //describes collection of the tests
 
     it("Catalog - filters", function () {
         //get by value
-        
+    
         cy.url().should("include", "/catalog");
         
         //------------------ Athor filter -------------------------
         cy.get("input[value='Mark Manson']").click();
         cy.get("input[value='Mary Shelley']").click();
         //verify that book was found
-
-        cy.get(".g-4").get("#6").should("exist");
-        cy.get(".g-4").get("#14").should("exist");
+        cy.get(".g-2").get("#6").should("exist");
+        cy.get(".g-2").get("#14").should("exist");
 
         cy.get("input[value='Mark Manson']").click();
 
